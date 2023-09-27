62df523fea40bb61f6ee8cbfdc648ed0eed31e5e
Jacob Lundholm <jal@intelligentsystems.dk>
2023-09-27 18:04:27 -0500

    Added starship installation script

---
+++ install-starship.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+RC='\e[0m'
+RED='\e[31m'
+YELLOW='\e[33m'
+GREEN='\e[32m'
+
+command_exists () {
+    command -v $1 >/dev/null 2>&1;
+}
+
+checkEnv() {
+    ## Check for requirements.
+    REQUIREMENTS='curl groups sudo'
+    if ! command_exists ${REQUIREMENTS}; then
+        echo -e "${RED}To run me, you need: ${REQUIREMENTS}${RC}"
+        exit 1
+    fi
+
+    ## Check Package Handeler
+    PACKAGEMANAGER='apt dnf pacman'
+    for pgm in ${PACKAGEMANAGER}; do
+        if command_exists ${pgm}; then
+            PACKAGER=${pgm}
+            echo -e "Using ${pgm}"
+        fi
+    done
+
+    if [ -z "${PACKAGER}" ]; then
+        echo -e "${RED}Can't find a supported package manager"
+        exit 1
+    fi
+
+
+    ## Check if the current directory is writable.
+    INSTALL_PATH="$(dirname "$(realpath "$0")")"
+    if [[ ! -w ${INSTALL_PATH} ]]; then
+        echo -e "${RED}Can't write to ${INSTALL_PATH}${RC}"
+        exit 1
+    fi
+
+    ## Check SuperUser Group
+    SUPERUSERGROUP='wheel sudo'
+    for sug in ${SUPERUSERGROUP}; do
+        if groups | grep ${sug}; then
+            SUGROUP=${sug}
+            echo -e "Super user group ${SUGROUP}"
+        fi
+    done
+
+    ## Check if member of the sudo group.
+    if ! groups | grep ${SUGROUP} >/dev/null; then
+        echo -e "${RED}You need to be a member of the sudo group to run me!"
+        exit 1
+    fi
+    
+}
+
+installDepend() {
+    ## Check for dependencies.
+    DEPENDENCIES='autojump bash bash-completion tar bat'
+    echo -e "${YELLOW}Installing dependencies...${RC}"
+    if [[ $PACKAGER == "pacman" ]]; then
+        if ! command_exists yay; then
+            echo "Installing yay..."
+            sudo ${PACKAGER} --noconfirm -S base-devel
+            $(cd /opt && sudo git clone https://aur.archlinux.org/yay-git.git && sudo chown -R ${USER}:${USER} ./yay-git && cd yay-git && makepkg --noconfirm -si)
+        else
+            echo "Command yay already installed"
+        fi
+    	yay --noconfirm -S ${DEPENDENCIES}
+    else 
+    	sudo ${PACKAGER} install -yq ${DEPENDENCIES}
+    fi
+}
+
+installStarship(){
+    if command_exists starship; then
+        echo "Starship already installed"
+        return
+    fi
+
+    if ! curl -sS https://starship.rs/install.sh|sh;then
+        echo -e "${RED}Something went wrong during starship install!${RC}"
+        exit 1
+    fi
+}
+
+installConfig() {
+    ## Check if a bashrc file is already there.
+    BASHRC_DIR="${HOME}/.bashrc.d"
+    if [[ ! -e ${BASHRC_DIR} ]]; then
+    	echo -e "${RED}${BASHRC_DIR} folder does not exist, please create it${RC}"
+    	exit 1
+    fi
+   
+    echo -e "${YELLOW}Copying config files (${INSTALL_PATH})...${RC}"
+    cp -a ${INSTALL_PATH}/.bashrc.d ${HOME}/
+    cp ${INSTALL_PATH}/starship.toml ${HOME}/.config/
+}
+
+checkEnv
+installDepend
+installStarship
+if installConfig; then
+    echo -e "${GREEN}Done!\nrestart your shell to see the changes.${RC}"
+else
+    echo -e "${RED}Something went wrong!${RC}"
+fi
